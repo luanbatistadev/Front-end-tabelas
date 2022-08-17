d637d862be1c31d2a3d338c1ed384debdd701365
luanbatistadev <luanbatista904@gmail.com>
2022-08-16 21:21:19 -0400

    Carlos pare de aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- gitpush.sh
+++ gitpush.sh
@@ -1,13 +1,10 @@
 git add .
 
-echo 'Enter the commit message:'
+echo 'Adicione o comentário do commit'
 read commitMessage
 
 git commit -m "$commitMessage"
 
-echo 'Enter the name of the branch:'
-read branch
-
-git push origin $branch
+git push
 
 read
